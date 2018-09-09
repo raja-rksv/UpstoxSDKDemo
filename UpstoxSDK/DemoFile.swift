60bcc8b794dc58bb9ed01a8cd4897b7dd9578954
Raj Anand <raj.anand@rksv.in>
2018-09-09 23:25:51 +0530

    Initial commit

---
+++ UpstoxSDK/DemoFile.swift
@@ -0,0 +1,33 @@
+//
+//  DemoFile.swift
+//  UpstoxSDK
+//
+//  Created by Raj Anand on 9/9/18.
+//  Copyright © 2018 Upstox. All rights reserved.
+//
+
+import Foundation
+public class UpstoxSDK {
+    //1.
+    private var isDebug: Bool!
+    
+    //2.
+    public init() {
+        self.isDebug = false
+    }
+    
+    //3.
+    public func setup(isDebug: Bool) {
+        self.isDebug = isDebug
+        print("Project is in Debug mode: \(isDebug)")
+    }
+    
+    //4.
+    public func YPrint<T>(value: T) {
+        if self.isDebug == true {
+            print(value)
+        } else {
+            //Do any stuff for production mode
+        }
+    }
+}
---
+++ UpstoxSDK/UpstoxSDK.h
@@ -0,0 +1,19 @@
+//
+//  UpstoxSDK.h
+//  UpstoxSDK
+//
+//  Created by Raj Anand on 9/9/18.
+//  Copyright © 2018 Upstox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UpstoxSDK.
+FOUNDATION_EXPORT double UpstoxSDKVersionNumber;
+
+//! Project version string for UpstoxSDK.
+FOUNDATION_EXPORT const unsigned char UpstoxSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UpstoxSDK/PublicHeader.h>
+
+
